8a57260389e44e7f806c4e268688d6cf47162060
triniti1 <gillig012@gmail.com>
2025-08-13 22:58:58 +0300

    db connection string in AppSettings file

---
+++ A2C.CRM.Api/A2C.CRM.Api/Data/AppDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace A2C.CRM.Api.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
